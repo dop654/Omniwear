9d823f202555f2e17e94d599f7e8f066b17d5c96
Fabio <f.nicuta@studenti.unisa.it>
2026-07-17 15:38:49 +0200

    Creazione di AdminOrdiniServlet

---
+++ src/main/java/omniwear/control/AdminHomeServlet.java
@@ -0,0 +1,23 @@
+package omniwear.control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admin/home")
+public class AdminHomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/admin/dashboard.jsp").forward(request, response);
+		return;
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/omniwear/control/AdminOrdiniServlet.java
@@ -0,0 +1,79 @@
+package omniwear.control;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import omniwear.dao.OrdineDAO;
+import omniwear.dao.OrdineDAOImpl;
+import java.util.ArrayList;
+import java.util.List;
+
+import omniwear.model.OrdineBean;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+@WebServlet("/admin/ordini")
+public class AdminOrdiniServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private OrdineDAO ordineDAO;
+	
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		super.init(servletConfig);
+		
+		DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
+		if(ds == null) {
+			throw new ServletException("DataSource non disponibile");
+		}
+		ordineDAO = new OrdineDAOImpl(ds);
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<String> errors = new ArrayList<>();
+		
+		try {
+			List<OrdineBean> ordini = (List<OrdineBean>) ordineDAO.doRetrieveAll(null);
+			
+			request.setAttribute("listaOrdini", ordini);
+            
+            request.getRequestDispatcher("/WEB-INF/admin/dashboard_ordini.jsp").forward(request, response);
+		} catch(SQLException e) {
+			errors.add(e.toString());
+		}
+		
+		 if(!errors.isEmpty()) {
+			request.setAttribute("errors", errors);
+		}
+		return;
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<String> errors = new ArrayList<>();
+		
+		int newStato = Integer.parseInt(RegisterServlet.validateField(request.getParameter("stato"), "stato", errors));
+		int idOrder = Integer.parseInt(RegisterServlet.validateField(request.getParameter("id_ordine"), "id ordine", errors));
+		
+		try {
+			ordineDAO.doUpdateStato(idOrder, newStato);
+			
+			response.sendRedirect(request.getContextPath() + "/admin/ordini");
+			
+		} catch(SQLException e) {
+			errors.add(e.toString());
+		}
+		
+		if(!errors.isEmpty()) {
+			request.setAttribute("errors", errors);
+		}
+		return;
+	}
+
+}
